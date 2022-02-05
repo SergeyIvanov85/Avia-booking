68e2e8c0cc9dd4a8a0be30a8f9a8d0ab81a72d81
Sergei Ivanov <koldyn85@yandex.ru>
2022-02-05 11:10:56 +0300

    3rd lesson

--- script/modules/airplane.js
+++ script/modules/airplane.js
@@ -106,12 +106,42 @@ const createAirplane = (title, tourData) => {
 
 };
 
+const checkSeat = (form, data) => {
+  form.addEventListener('change', () => {
+    const formData = new FormData(form);
+    const checked = [...formData].map(([, value]) => value);
+
+    if (checked.length === data.length) {
+      [...form].forEach(item => {
+        if (item.checked === false && item.name === 'seat') {
+          item.disabled = true;
+        }
+      })
+    }
+  });
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const booking = [...formData].map(([, value]) => value);
+
+    for(let i = 0; i < data.length; i++) {
+      data[i].seat = booking[i];
+    }
+
+    console.log(data);
+  });
+};
+
 
 const airplane = (main, data, tourData) => {
   const title = `Выберете ${declOfNum(data.length, ['место','места','мест'])}`;
 
+  const choiseForm = createAirplane(title, tourData);
 
-  main.append(createAirplane(title, tourData));
+  checkSeat(choiseForm, data);
+
+  main.append(choiseForm);
  
 };
 
